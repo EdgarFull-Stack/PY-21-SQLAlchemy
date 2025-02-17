7debfa114c82616dc7a77f1fbe6f71f3fe5c838f
Edgar Lipnevic <edgarlipnevic@gmail.com>
2025-02-17 19:30:38 +0200

    First commit with 1 Task

---
+++ tasks.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, create_engine, Integer, String
+from sqlalchemy.orm import declarative_base
+# Task 1
+engine = create_engine("sqlite:///mokykla.db")
+Base = declarative_base()
+
+class Mokinys(Base):
+    __tablename__ = 'mokiniai'
+    id = Column(Integer, primary_key=True)
+    vardas = Column(String)
+    pavarde = Column(String)
+    klase = Column(Integer)
+
+class Mokytojas(Base):
+    __tablename__ = 'mokytojai'
+    id = Column(Integer, primary_key=True)
+    vardas = Column(String)
+    pavarde = Column(String)
+    dalykas = Column(String)
+
+Base.metadata.create_all(engine)
